38c6c982416fc90405b65469673b7f418247d1f2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-05-01 22:12:46 +0200

    inital

---
+++ background.js
@@ -0,0 +1,39 @@
+/* global browser */
+
+const locked_tabs = new Set();
+
+browser.browserAction.onClicked.addListener((tab, clickData) => {
+  if (locked_tabs.has(tab.id)) {
+    locked_tabs.delete(tab.id);
+    browser.browserAction.setBadgeText({tabId: tab.id, text: "" });
+  } else {
+    locked_tabs.add(tab.id);
+    browser.browserAction.setBadgeText({tabId: tab.id, text: "🔒" });
+  }
+});
+
+async function onBeforeWebRequest(requestDetails) {
+  if (locked_tabs.has(requestDetails.tabId)) {
+    const tmp_tab = await browser.tabs.get(requestDetails.tabId);
+    tmp_tab_origin = new URL(tmp_tab.url).origin;
+
+    if (!requestDetails.url.startsWith(tmp_tab_origin)) {
+      browser.tabs.create({
+        active: true,
+        url: requestDetails.url,
+      });
+      return { cancel: true };
+    }
+  }
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  onBeforeWebRequest,
+  { urls: ["<all_urls>"], types: ["main_frame"] },
+  ["blocking"]
+);
+
+
+// default 
+	  browser.browserAction.setBadgeText({text: "" });
+          browser.browserAction.setBadgeBackgroundColor({color: 'green'});
