9fb1ddcc03fe67ae1eff16b3372c4dd22ce1f8f5
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-05-13 17:18:00 +0200

    fix: re-map actively created tabs to behave as expected after locking

--- background.js
+++ background.js
@@ -1,28 +1,42 @@
 /* global browser */
 
-const locked_tabs = new Set();
+const tabId2Origin = new Map();
 
 browser.browserAction.onClicked.addListener((tab) => {
-  if (locked_tabs.has(tab.id)) {
-    locked_tabs.delete(tab.id);
+  if (tabId2Origin.has(tab.id)) {
+    tabId2Origin.delete(tab.id);
     browser.browserAction.setBadgeText({ tabId: tab.id, text: "" });
   } else {
-    locked_tabs.add(tab.id);
+    tabId2Origin.set(tab.id, new URL(tab.url).origin);
     browser.browserAction.setBadgeText({ tabId: tab.id, text: "🔒" });
   }
 });
 
 async function onBeforeWebRequest(requestDetails) {
-  if (locked_tabs.has(requestDetails.tabId)) {
-    const tmp_tab = await browser.tabs.get(requestDetails.tabId);
-    const tmp_tab_origin = new URL(tmp_tab.url).origin;
+  if (tabId2Origin.has(requestDetails.tabId)) {
+    const locked_origin = tabId2Origin.get(requestDetails.tabId);
 
-    if (!requestDetails.url.startsWith(tmp_tab_origin)) {
+    if (!requestDetails.url.startsWith(locked_origin)) {
       browser.tabs.create({
         active: true,
         url: requestDetails.url,
       });
       return { cancel: true };
+    }
+  } else {
+    const tab = await browser.tabs.get(requestDetails.tabId);
+
+    if (tab.openerTabId && tabId2Origin.has(tab.openerTabId)) {
+      const tab_origin = new URL(requestDetails.url).origin;
+      const opener_origin = tabId2Origin.get(tab.openerTabId);
+
+      if (tab_origin === opener_origin) {
+        browser.tabs.update(tab.openerTabId, {
+          active: true,
+          url: requestDetails.url,
+        });
+        browser.tabs.remove(requestDetails.tabId);
+      }
     }
   }
 }
@@ -36,3 +50,17 @@ browser.webRequest.onBeforeRequest.addListener(
 // default
 browser.browserAction.setBadgeText({ text: "" });
 browser.browserAction.setBadgeBackgroundColor({ color: "green" });
+
+// update icon status
+browser.tabs.onUpdated.addListener(
+  (tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+      if (tabId2Origin.has(tab.id)) {
+        browser.browserAction.setBadgeText({ tabId: tab.id, text: "🔒" });
+      } else {
+        browser.browserAction.setBadgeText({ tabId: tab.id, text: "" });
+      }
+    }
+  },
+  { properties: ["status"] }
+);
