6780d4267e6ba7ff40e396d968c30cfd63d8fce3
igorlogius <67047467+igorlogius@users.noreply.github.com>
2023-06-05 10:57:19 +0200

    feat: set default to auto-locking

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
 /* global browser */
 
 const tabId2Origin = new Map();
+const unlockedTabs = new Set();
 
 browser.browserAction.onClicked.addListener((tab) => {
   if (tabId2Origin.has(tab.id)) {
+    unlockedTabs.add(tab.id);
     tabId2Origin.delete(tab.id);
     browser.browserAction.setBadgeText({ tabId: tab.id, text: "" });
   } else {
+    unlockedTabs.delete(tab.id);
     tabId2Origin.set(tab.id, new URL(tab.url).origin);
     browser.browserAction.setBadgeText({ tabId: tab.id, text: "🔒" });
   }
@@ -39,6 +42,15 @@ browser.browserAction.setBadgeBackgroundColor({ color: "green" });
 // update icon status
 browser.tabs.onUpdated.addListener(
   (tabId, changeInfo, tab) => {
+    if (!unlockedTabs.has(tab.id)) {
+      if (
+        typeof changeInfo.url === "string" &&
+        changeInfo.url.startsWith("http")
+      ) {
+        // auto lock
+        tabId2Origin.set(tab.id, new URL(tab.url).origin);
+      }
+    }
     if (changeInfo.status === "complete") {
       if (tabId2Origin.has(tab.id)) {
         browser.browserAction.setBadgeText({ tabId: tab.id, text: "🔒" });
@@ -47,7 +59,7 @@ browser.tabs.onUpdated.addListener(
       }
     }
   },
-  { properties: ["status"] }
+  { properties: ["status", "url"] }
 );
 
 browser.tabs.onRemoved.addListener((tabId) => {
